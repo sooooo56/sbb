a4773956852ae30aafa06e4ff0ab6928362a9545
sooooo56 <sooooo4645@gmail.com>
2024-10-24 00:46:26 +0900

    sbb 2-14 완료

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -16,11 +16,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AnswerController {
 
     private final QuestionService questionService;
+    private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value = "contnet") String content){
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
 
         Question question = this.questionService.getQuestion(id);
+
+        this.answerService.create(question,content);
         return String.format("redirect:/question/detail/%s",id);
     }
 
