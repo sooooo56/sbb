bd6b48175cf0ecb702a689d74062807f58259644
sooooo56 <sooooo4645@gmail.com>
2024-11-05 00:05:19 +0900

    sbb 3-2 완료

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -2,9 +2,14 @@ package com.mysite.sbb.question;
 
 import com.mysite.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,8 +19,12 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList(){
-        return this.questionRepository.findAll();
+    public Page<Question> getList(int page){
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+
+        Pageable pageable = PageRequest.of(page,10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
 
     public Question getQuestion(Integer id) {
@@ -34,4 +43,6 @@ public class QuestionService {
         q.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q);
     }
+
+
 }
