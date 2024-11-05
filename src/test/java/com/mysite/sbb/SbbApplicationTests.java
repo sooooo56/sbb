ea758b84685628cc9c6d1638e50e0c2d3f403a7e
sooooo56 <sooooo4645@gmail.com>
2024-11-05 21:26:47 +0900

    로그인/회원가입

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -24,7 +24,7 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa() {
-		for(int i = 1; i <= 300; i++){
+		for(int i = 1; i <= 20; i++){
 			String subject = String.format("테스트 데이터 입니다:[%03d]",i);
 			String content = "내용";
 			this.questionService.create(subject,content);
